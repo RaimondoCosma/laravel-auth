cf555c63e954f8511d5d20dcb4d31ffb13524c98
RaimondoCosma <raimondo.cosma@gmail.com>
2023-01-25 18:14:13 +0100

    Aggiunge index e show

---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h1>Lista Progetti</h1>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Titolo</th>
+                <th scope="col">Descrizione</th>
+                <th scope="col">Slug</th>
+                <th scope="col">Link</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($projects as $project)
+                <tr>
+                    <th scope="row">{{ $project->id }}</th>
+                    <td>{{ $project->title }}</td>
+                    <td>{{ $project->description }}</td>
+                    <td>{{ $project->slug }}</td>
+                    <td><a class="btn btn-primary" href="{{ route('admin.projects.show', $project->id) }}">Dettagli</a></td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endsection
